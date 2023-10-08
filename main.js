851eb7aeae35d860372f3b34f076ab416f58bf46
Charlie <chrlie.w@proton.me>
2023-10-08 16:50:17 +0100

    changed event listener to form submit

--- main.js
+++ main.js
@@ -20,44 +20,49 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-const submit = document.querySelector("#submit");
-submit.addEventListener("click", () => {
+const form = document.querySelector(".modal-form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
     const title = document.querySelector("#title").value;
     const author = document.querySelector("#author").value;
     const pages = document.querySelector("#pages").value;
-    const read = document.querySelector("#read").value;
+    const read = document.querySelector("#read").checked;
+
     const book = new Book(title, author, pages, read);
     addBookToLibrary(book);
     addLibraryToPage();
+    console.log(book) 
     modal.close();
+
 });
 
 function addLibraryToPage() {
-  const library = document.querySelector(".main");
-  library.innerHTML = "";
-  myLibrary.forEach((book) => {
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("book-card");
-    const title = document.createElement("h2");
-    title.textContent = book.title;
-    const author = document.createElement("h3");
-    author.textContent = book.author;
-    const pages = document.createElement("h3");
-    pages.textContent = book.pages;
-    const read = document.createElement("h3");
-    read.textContent = book.read;
-    const remove = document.createElement("button");
-    remove.classList.add("remove");
-    remove.textContent = "Remove";
-    remove.addEventListener("click", () => {
-      myLibrary.splice(myLibrary.indexOf(book), 1);
-      addLibraryToPage();
+    const library = document.querySelector(".main");
+    library.innerHTML = "";
+    myLibrary.forEach((book) => {
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+        const title = document.createElement("h2");
+        title.textContent = book.title;
+        const author = document.createElement("h3");
+        author.textContent = book.author;
+        const pages = document.createElement("h3");
+        pages.textContent = book.pages;
+        const read = document.createElement("h3");
+        read.textContent = book.read;
+        const remove = document.createElement("button");
+        remove.classList.add("remove");
+        remove.textContent = "Remove";
+        remove.addEventListener("click", () => {
+        myLibrary.splice(myLibrary.indexOf(book), 1);
+        addLibraryToPage();
+        });
+        bookCard.appendChild(title);
+        bookCard.appendChild(author);
+        bookCard.appendChild(pages);
+        bookCard.appendChild(read);
+        bookCard.appendChild(remove);
+        library.appendChild(bookCard);
     });
-    bookCard.appendChild(title);
-    bookCard.appendChild(author);
-    bookCard.appendChild(pages);
-    bookCard.appendChild(read);
-    bookCard.appendChild(remove);
-    library.appendChild(bookCard);
-  });
 }
