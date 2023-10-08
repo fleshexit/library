9fe722c5c6ccdeb1252f1849a8ea5be53a592089
Charlie <chrlie.w@proton.me>
2023-10-08 18:03:19 +0100

    image on card

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+
 const openModal = document.querySelector("#open-modal");
 const modal = document.querySelector(".modal");
 
@@ -40,17 +41,26 @@ form.addEventListener("submit", (e) => {
 function addLibraryToPage() {
     const library = document.querySelector(".main");
     library.innerHTML = "";
+
     myLibrary.forEach((book) => {
         const bookCard = document.createElement("div");
         bookCard.classList.add("book-card");
+
+        const img = document.createElement("img");
+        img.src = "./images/bookshelf.jpg";
+
         const title = document.createElement("h2");
-        title.textContent = book.title;
+        title.textContent = '"' + book.title + '"';
+
         const author = document.createElement("h3");
         author.textContent = book.author;
+
         const pages = document.createElement("h3");
-        pages.textContent = book.pages;
+        pages.textContent = `${book.pages} pages`
+
         const read = document.createElement("h3");
-        read.textContent = book.read;
+        read.textContent = ((book.read == true) ? "read" : "not read");
+
         const remove = document.createElement("button");
         remove.classList.add("remove");
         remove.textContent = "Remove";
@@ -58,6 +68,8 @@ function addLibraryToPage() {
         myLibrary.splice(myLibrary.indexOf(book), 1);
         addLibraryToPage();
         });
+
+        bookCard.appendChild(img);
         bookCard.appendChild(title);
         bookCard.appendChild(author);
         bookCard.appendChild(pages);
@@ -66,3 +78,10 @@ function addLibraryToPage() {
         library.appendChild(bookCard);
     });
 }
+
+
+// <a href="https://www.freepik.com/free-photo/antique-book-shelf-vintage-background_21631166.htm#query=bookshelf&position=20&from_view=keyword&track=sph">Image by rawpixel.com</a> on Freepik
+
+// <a href="https://www.freepik.com/free-vector/book-shelves-dtcorative-colorful-icon-poster_2871137.htm#query=bookshelf&position=2&from_view=keyword&track=sph">Image by macrovector</a> on Freepik
+
+// <a href="https://www.freepik.com/free-vector/white-bookshelf-mockup-books-shelf-library_8497568.htm#query=bookshelf&position=3&from_view=keyword&track=sph">Image by upklyak</a> on Freepik
